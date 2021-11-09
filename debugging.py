e1710f15265f390bcde1c9e7922573fb927d93bc
Luis Santamaria <luisignaciosantamariap@gmail.com>
2021-11-08 23:35:21 -0500

    Clase = Debugging - Aprendimos a hacer debugging/depurar con las herramientas de VS Code - debugging.py - Clase 14

---
+++ debugging.py
@@ -0,0 +1,18 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+      # if num % i == 1:
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    num = int(input("Ingresa un número: "))
+    print(divisors(num))
+    print("Program ends")
+
+
+
+if __name__ == "__main__":
+    run()
