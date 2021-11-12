82c5dc6240b5611973f5bb70271e912c37797a87
Luis Santamaria <luisignaciosantamariap@gmail.com>
2021-11-12 12:08:50 -0500

    Clase = Poniendo a prueba el manejo de excepciones - Realizamos un reto combinando try-except y raise - Modificamos debudding.py - Clase 16

--- debugging.py
+++ debugging.py
@@ -1,16 +1,23 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
-      # if num % i == 1:
+    # if num % i == 1:
         if num % i == 0:
             divisors.append(i)
     return divisors
 
 
+
+
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Program ends")
+    try:
+        num = int(input("Ingresa un número: "))
+        if (num < 0):
+            raise ValueError(str(num) + " --> Ingresa un numero positivo")
+        print(divisors(num))
+        print("Program ends")
+    except ValueError as re:
+        print(re)
 
 
 
